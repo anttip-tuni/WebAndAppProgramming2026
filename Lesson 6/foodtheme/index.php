cf5d28db7bf66515c026abb471611bbe3bd286ad
AnttiPerala <99724071+AnttiPerala@users.noreply.github.com>
2026-04-24 11:28:51 +0300

    lunch

--- Lesson 6/foodtheme/index.php
+++ Lesson 6/foodtheme/index.php
@@ -9,15 +9,31 @@
 
     <?php 
     
-       global $wpdb;
+       global $wpdb; /* Access the built in WordPress database object, which simplifies database queries */
 
-       $rowresults = $wpdb->get_results("SELECT name FROM foodsenglish");
+       $rowresults = $wpdb->get_results("SELECT name FROM foodsenglish"); /* 
+       1. we are creating a variable called $rowresults, which will contain the results of our query. 
+
+       2. we are assigning to it the results of the query, which we get by calling the get_results method of the $wpdb object.
+
+       3. the query itself is a simple SQL query, which selects the name column from the foodsenglish table.
+
+       */
 
        foreach ($rowresults as $row){
+        /* We are using a PHP foreach loop to iterate through the results. We are deciding to call each individual row $row */
         ?>
-        <p><?php echo $row->name; ?></p>
+        <!-- Here we closed the php tag so that we can use regular HTML to create some p tags -->
+        <p>
+        
+            <?php echo $row->name; ?>
+            <!-- we are taking the name property from the current row. Echo will write it to the page -->
+
+        
+        </p>
         <?php
        }
+       /* We are using PHP again just to close the foreach loop  */
 
     ?>
 
